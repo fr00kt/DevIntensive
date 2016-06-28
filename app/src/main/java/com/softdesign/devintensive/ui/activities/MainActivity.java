5a12285aa128ff03f976a4dd35ec25ab8b6e4cd2
fr00kt <fr00kt@yahoo.com>
2016-06-28 17:21:57 +0500

    Реализовал и залогировал жизненный цикл активити (расставил логирование соответствующее жизненному циклу)

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,14 +2,102 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    /**
+     * Метод вызывается при создании активити после изменения конфигурации/возврата к текушей
+     * активити после его уничтожения.
+     *
+     * в данном методе инициализируются/производится:
+     * - UI пользовательский интерфейс (статика)
+     * - инициализация статических данных активити
+     * - связь данных со списками (инициализация адаптеров)
+     *
+     * Не запускать длительные операции по работе с данными в onCreate() !!!
+     *
+     * @param savedInstanceState - объект со значениями сохраненными в Bundle - состояние UI
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    /**
+     * Метод вызывается при старте активити перед моментом того как UI станет достепен пользователю
+     * как правило в данном методе происходит регистрация подписки на событиея остановка которых была
+     * произведена в onStop()
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    /**
+     * Метод вызывается когда активити становится доступен пользователю для взаимодействия
+     * в данном методе как правило происходит запуск анимаций/аудио/видео/запуск BroadcastReceiver
+     * необходимых для реализации UI логика/запуск выполнение потоков и т.д.
+     * метод должен быть максимально легковесным для максимальной отзывчивости UI
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    /**
+     * Метод вызывается когда текущая активити теряет фокус но остается видимой(всплытие
+     * диалогового окна/частичное перекрытие другой активити и т.д)
+     * <p/>
+     * в данном методе реализуют сохранение легковесных UI данных/анимаций/аудио/видео  и т.д
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    /**
+     * Метод вызывается когда активити становится невидимым для пользователя.
+     * в данном методе происходит отписка от событий, остановка сложных анимаций, сложные операции по
+     * сохранению данных/прерывание запущенных потоков и т.д.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    /**
+     * Метод вызывается при окончании работы активити (когда это происходит системно или после вызова
+     * метода finish())
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    /**
+     * Метод вызывается при рестарте активити/возобнавлении работы после вызова метода onStop()
+     * в данном методе реализуется спецификация бизнес логики которая должна быть реализована именно
+     * при рестарте активности - например запрос к серверу который необходимо вызвать при возвращении
+     * из другой активности (обновление данных, подписка на определенное событие проинициализированное
+     * на другом экране/специфическая бизнес логика завязанная именно на перезапуске активити)
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 }
